25ac7c38573416301cbabb532020e055c69a6a69
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-31 19:39:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,10 +19,6 @@ end
 def get_japanese_emoticon(file, emoticon)
   load_library(file)
   
-  if dictionary["get_emoticon"].has_key?(emoticon)
-  dictionary["get_emoticon"][icons]
-  
-  end
 end
 ################################
 
