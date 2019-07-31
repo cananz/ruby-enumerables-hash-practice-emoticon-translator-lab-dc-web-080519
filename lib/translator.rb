f2a62fed775fe2df53d3f71431d02b15443a63cb
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-31 19:00:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,12 @@ require "yaml"
 
 def load_library(file)
   emoticons = YAML.load_file(file)
-
-dictionary = {
+  dictionary = {
   "get_meaning" => {},
   "get_emoticon" => {}
-}
-emoticons.each do 
-
+    
+  }
+  emoticons.each do 
 end
 
 def get_japanese_emoticon
